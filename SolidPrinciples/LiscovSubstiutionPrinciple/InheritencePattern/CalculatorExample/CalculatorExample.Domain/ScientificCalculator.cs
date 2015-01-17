09923f4ff21518c93a1c3736d87dd73914a716e2
Darren Ackers <ackers86@hotmail.com>
2015-01-17 22:57:31 +0000

    Updated naming in the file path, CalculatorExample no longer has .tests after it

---
+++ SolidPrinciples/LiscovSubstiutionPrinciple/InheritencePattern/CalculatorExample/CalculatorExample.Domain/ScientificCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorExample.Domain
+{
+    public class ScientificCalculator : Calculator
+    {
+        public double SquareRoot(int number)
+        {
+            return Math.Sqrt(number);
+        }
+    }
+}
