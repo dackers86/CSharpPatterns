d0a0163bb26ceac94425da852d379f24f293bbdc
Darren Ackers <ackers86@hotmail.com>
2015-01-18 15:02:15 +0000

    Added a tets for adding a Single Ticket

---
+++ SolidPrinciples/LiscovSubstiutionPrinciple/StrategyPattern/TicketCalculator/TicketCalculator.Domain/TicketCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketCalculator.Domain
+{
+    public class TicketCalculator
+    {
+        public double Calculate()
+        {
+            return 10.00;
+        }
+    }
+}
---
+++ SolidPrinciples/LiscovSubstiutionPrinciple/StrategyPattern/TicketCalculator/TicketCalculator.Tests/TicketCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicketCalculator.Tests
+{
+    [TestClass]
+    public class TicketCalculatorTests
+    {
+        [TestMethod]
+        public void WhenAddingASingleTicket_ItShouldReturnTheCorrectAmount()
+        {
+            var ticketCalculator = new TicketCalculator.Domain.TicketCalculator();
+            var result = ticketCalculator.Calculate();
+            Assert.AreEqual(result, 10.00);
+        }
+}
