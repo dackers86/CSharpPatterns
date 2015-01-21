a1c6c60034cf44620060147ae1599bff47340352
Darren Ackers <ackers86@hotmail.com>
2015-01-21 21:27:02 +0000

    Added MessageQueue folder structure for RabbitMQ

---
+++ SolidPrinciples/LiscovSubstiutionPrinciple/StrategyPattern/TicketCalculator/TicketCalculator.TicketStrategy/TicketStrategy/SingleTicket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketCalculator.TicketStrategy
+{
+    public class SingleTicket : ITicketStrategy
+    {
+        public double CalculateBasePrice()
+        {
+            return 10.00;
+        }
+    }
+}
